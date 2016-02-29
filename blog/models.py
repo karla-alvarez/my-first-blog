48e7f0825ca5d9db553fb3435dd5b07ddd828a5c
Karla Alvarez <karla@SOC-AndyG.local>
2016-02-29 14:06:11 -0600

    Agregue ciclo en plantilla

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Post(models.Model):
 	author = models.ForeignKey( 'auth.User' )
-	tittle = models.CharField(max_length=200)
+	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(
 		    default=timezone.now)
@@ -15,7 +15,7 @@ class Post(models.Model):
 		self.save()
 
 	def __str__(self):
-		return self.tittle
+		return self.title
 
 
 # Create your models here.
